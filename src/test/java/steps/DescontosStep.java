76fdc195f479d1be10917b01a85f7598334a522c
Eduardo Santos <eduardosantosbox@gmail.com>
2023-01-09 19:38:01 -0300

    Criação da classe DescontosStep

---
+++ src/test/java/steps/DescontosStep.java
@@ -0,0 +1,34 @@
+package steps;
+
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Quando;
+import cucumber.api.java.pt.Entao
+
+public class DescontosStep {
+
+    @Dado("^que estou no site da QAzando$")
+    public void que_estou_no_site_da_QAzando() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Quando("^eu preencho meu e-mail$")
+    public void eu_preencho_meu_e_mail() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Quando("^clico em ganhar cupom$")
+    public void clico_em_ganhar_cupom() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Entao("^eu vejo o codigo de desconto$")
+    public void eu_vejo_o_codigo_de_desconto() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+
+}
---
+++ target/test-classes/features/descontos.feature
@@ -0,0 +1,12 @@
+# language: pt
+
+  Funcionalidade: Receber desconto da QAzando
+    Eu como usuário da QAzando
+    quero recer cupom de desconto
+    para comprar um curso com valor reduzido
+
+    Cenário: Visualizar código de desconto
+      Dado que estou no site da QAzando
+      Quando eu preencho meu e-mail
+      E clico em ganhar cupom
+      Então eu vejo o código de desconto
