ed6963556ef5a7dde68a871731b319697c59ae5b
Eduardo Santos <eduardosantosbox@gmail.com>
2023-01-10 10:50:05 -0300

    Atualização

--- src/test/java/steps/DescontosStep.java
+++ src/test/java/steps/DescontosStep.java
@@ -2,32 +2,32 @@ package steps;
 
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Quando;
-import cucumber.api.java.pt.Entao
+import cucumber.api.java.pt.Entao;
 
 public class DescontosStep {
 
     @Dado("^que estou no site da QAzando$")
-    public void que_estou_no_site_da_QAzando() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void que_estou_no_site_da_QAzando() {
+
+        System.out.println("Passou 1");
     }
 
     @Quando("^eu preencho meu e-mail$")
-    public void eu_preencho_meu_e_mail() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void eu_preencho_meu_e_mail() {
+
+        System.out.println("Passou 2");
     }
 
     @Quando("^clico em ganhar cupom$")
-    public void clico_em_ganhar_cupom() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void clico_em_ganhar_cupom() {
+
+        System.out.println("Passou 3");
     }
 
     @Entao("^eu vejo o codigo de desconto$")
-    public void eu_vejo_o_codigo_de_desconto() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void eu_vejo_o_codigo_de_desconto() {
+
+        System.out.println("Passou 4");
     }
 
 
--- src/test/resources/features/descontos.feature
+++ src/test/resources/features/descontos.feature
@@ -2,10 +2,10 @@
 
   Funcionalidade: Receber desconto da QAzando
     Eu como usuario da QAzando
-    quero recer cupom de desconto
+    quero receber cupom de desconto
     para comprar um curso com valor reduzido
 
-    Cenario: rio: Visualizar código de desconto
+    Cenario: Visualizar codigo de desconto
       Dado que estou no site da QAzando
       Quando eu preencho meu e-mail
       E clico em ganhar cupom
